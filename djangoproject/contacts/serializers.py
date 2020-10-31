8d3a21d89f4c79b780c4bd1b59f3f3eb127c7618
Sarthak337 <54993520+Sarthak337@users.noreply.github.com>
2020-10-31 08:24:26 +0530

    Update serializers.py

--- djangoproject/contacts/serializers.py
+++ djangoproject/contacts/serializers.py
@@ -1,29 +1,8 @@
 from rest_framework import serializers
-from .models import Contact,Personal_Information,Pedigree,Social_media_link,Office_Detail
+from .models import Contact
 
 class ContactSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Contact
         fields = "__all__"
-class Personal_InformationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Personal_Information
-        fields = "__all__"
-class PedigreeSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Pedigree
-        fields = "__all__"
-class Social_media_linkSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Social_media_link
-        fields = "__all__"
-class Office_DetailSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Office_Detail
-        fields = "__all__"
-
